07a8cc265aa7a5a821733051342a8dd900ead672
maxgunchoi <chleorjs37@gmail.com>
2022-05-23 17:55:12 +0900

    계산기 테스트

--- __tests__/practice.test.js
+++ __tests__/practice.test.js
@@ -1,5 +1,9 @@
-describe("solution test",()=>{
+import { plus } from '../src/calculate';
+
+describe("더하기 테스트",()=>{
     it("test", ()=>{
-        expect(true).toEqual(true);
+        expect(plus(3,4)).toEqual(7);
+        expect(plus(3,1)).not.toStrictEqual(7);
     })
 });
+
